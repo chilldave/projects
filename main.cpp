e643d2a5ba960ae8e6dc4d334e5aba9c188937f1
denisvasquez <gomezdenis150@gmail.com>
2023-03-02 16:32:29 -0600

    almost ended

--- main.cpp
+++ main.cpp
@@ -3,25 +3,31 @@
 
 using namespace std;
 
-void DatosGenerales();
-void ShowData();
-
+// struct about of academic information
 struct AcademicInformation{
     string sede;
     string career;
     string course;
     char section[2];
 };
+
+// struct about basic information, has the same struct that academic information
 struct BasicInformation : AcademicInformation {
     string nombre;
     int age;
     string state;
     string address;
     string email;
-}student;
+};
 
+
+void DatosGenerales(); // function that ask the basic datas of the user
+void ShowData(BasicInformation student); // fuction that show the information of the user
+
+// function main that start our program
 int main(){
 
+    // invoque the function for the basic information
     DatosGenerales();
     
     return 0;
@@ -30,12 +36,18 @@ int main(){
 void DatosGenerales(){
     bool loop = true;
 
+    // we create a new instance of the struct
+    BasicInformation student;
+
     while (loop) {
 
-        system("clear");
-        system("figlet -c 'Grupo:'");
+        system("clear"); // clear the terminal
+
+        // show the name group
+        system("figlet -c 'Grupo:'"); 
         system("figlet -c 'Los hackers'");
 
+        // ask the information
         cout << "Ingrese su nombre completo: "; getline(cin, student.nombre,'\n');
 
         cout << "Ingrese su edad: "; cin >> student.age;
@@ -45,39 +57,49 @@ void DatosGenerales(){
         cout << "Ingrese su direccion "; getline(cin, student.address,'\n');
         cout << "Ingrese su e-mail: "; getline(cin, student.email,'\n');
 
-        ShowData();
+        cout << "Ingrese su sede: "; getline(cin, student.sede,'\n');
+        cout << "Ingrese su carrera: "; getline(cin, student.career,'\n');
+        cout << "Ingrese su curso: "; getline(cin, student.course,'\n');
+        cout << "Ingrese su seccion: "; cin >> student.section;
 
+        // show the infomation
+        ShowData(student);
+
+        // ask to control the loop main
         string conf;
-
         while (conf != "n") {
-            cout << endl << "¿Deseas volver a intentarlo? [s/n]: "; cin >> conf;
+            cout << endl << "¿Deseas volver a intentarlo? [s/n]: "; cin >> conf; // we ask
             cin.ignore();
 
-            if (conf == "n") {
+            if (conf == "n") { // if the 'conf' is 'n' break the program
                 loop = false;
                 break;
             };
 
-            if (conf != "s") {
+            if (conf != "s") { // if the 'conf' not is 's' show a message
                 cout << "Comando no reconocido!!" << endl << endl;
             }
 
-            if (conf == "s") {
+            if (conf == "s") { // if the 'conf' is 's' continue with the program
                 break;
             }
         }        
     }
 
 }
-void ShowData(){
-    system("clear");
-    system("figlet -c 'Datos generales'");
+
+void ShowData(BasicInformation student){
+    system("clear"); // clear the terminal
+    system("figlet -c 'Datos generales'"); // show a message above of the information
 
     cout << "Nombre completo: " << student.nombre << endl;
     cout << "Edad: " << student.age << endl;
     cout << "Estado civil: " << student.state << endl;
     cout << "Direccion: "<< student.address << endl;
-    cout << "E-mail:  " << student.email << endl;
+    cout << "Sede: " << student.sede << endl;
+    cout << "Carrera: " << student.career << endl;
+    cout << "Curso: " << student.course << endl;
+    cout << "Sección: " << student.section << endl;
 
     
 }
