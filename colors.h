f66b059b5715b45cc37e15ef7b813390b2453ca8
davechill <cdcaalcamey@gmail.com>
2023-03-03 20:12:28 -0600

    update

--- colors.h
+++ colors.h
@@ -1,8 +1,6 @@
 
-
-
+//  Colors
 #define FGYellow "\x1B[33m"
 #define blank "\x1B[0m"
 #define FGGreen "\x1B[32m"
-
 #define BGBrightMagenta "\x1B[105m"
--- main.cpp
+++ main.cpp
@@ -47,10 +47,9 @@ void DatosGenerales(){
         system("figlet -c 'Grupo:'"); 
         system("figlet -c 'Byte Band'");
 
-
         // ask the information
         cout << FGYellow << "\n\tIngrese su nombre completo: "<< blank; getline(cin, student.nombre,'\n');
-        cout << FGYellow <<"\n\tIngrese su edad: "<< blank; cin >> student.age;
+        cout << FGYellow <<"\n\tIngrese su edad: "<< blank  ; cin >> student.age;
         cin.ignore();
         cout << FGYellow <<"\n\tIngrese su estado civil: "<< blank; getline(cin, student.state,'\n');
         cout << FGYellow <<"\n\tIngrese su direccion "<< blank; getline(cin, student.address,'\n');
@@ -64,21 +63,21 @@ void DatosGenerales(){
         ShowData(student);
 
         // ask to control the loop main
-        string conf;
-        while (conf != "n") {
+        char conf;
+        while (conf != 'n') {
             cout << endl << "¿Deseas volver a intentarlo? [s/n]: "; cin >> conf; // we ask
             cin.ignore();
 
-            if (conf == "n") { // if the 'conf' is 'n' break the program
+            if (conf == 'n') { // if the 'conf' is 'n' break the program
                 loop = false;
                 break;
             };
 
-            if (conf != "s") { // if the 'conf' not is 's' show a message
+            if (conf != 's') { // if the 'conf' not is 's' show a message
                 cout << "Comando no reconocido!!" << endl << endl;
             }
 
-            if (conf == "s") { // if the 'conf' is 's' continue with the program
+            if (conf == 's') { // if the 'conf' is 's' continue with the program
                 break;
             }
         }        
@@ -90,7 +89,7 @@ void ShowData(BasicInformation student){
     system("clear"); // clear the terminal
 
     system("figlet -c 'Registrado'"); // show a message above of the information
-    cout<<" ---------------------------------------------------------------------------------"<<endl;
+    cout<<" -----------------------------------------------------------------------------"<<endl;
     cout << FGYellow <<"\tNombre completo: "<< blank <<student.nombre << endl;
     cout << FGYellow <<"\n\tEdad: " << blank << student.age << endl;
     cout << FGYellow <<"\n\tEstado civil: " << blank << student.state << endl;
